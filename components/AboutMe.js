b95955d36c613238c299d57a3922959d059f7d71
Wagner Paulo <wagstalos@hotmail.com>
2023-04-07 21:01:37 -0300

    feat: tootip popover

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -23,11 +23,11 @@ export default function AboutMe() {
 
         <div className={styles.description}>
           <DiCode size="32" color="#656AF5" className={styles.icons} />
-          <h2>Prazer, sou Wagner Paulo</h2>
+          <h2>Prazer, sou Wagner Silva</h2>
           <p>
-            Formado em Design Gráfico (UNIP) , Desenvolvimento de Jogos Digitais
-            (ETEC) e cursando Pós-Graduação Desenvolvimento Fullstack de
-            Sistemas Modernos para Nuvem (Cloud Native). Apaixonado por
+            Formado em Design Gráfico (<span data-tooltip="2012">UNIP</span>) , Desenvolvimento de Jogos Digitais
+            (<span data-tooltip="2016">ETEC</span>) e cursando Pós-Graduação Desenvolvimento Fullstack de
+            Sistemas Modernos para Nuvem (<span data-tooltip="2023">UNESC</span>). Apaixonado por
             tecnologias, trabalho com produção de sites, E-commerce (Salesforce
             SFCC), WordPress, SEO, aplicativos, PWA e jogos digitais utlizando
             metodologias ágeis como Scrum e Kanban .
@@ -35,13 +35,13 @@ export default function AboutMe() {
 
           <h3>Skills</h3>
           <div className={styles.iconsHeader}>
-            <DiHtml5 size="32" className={styles.icons} />
-            <DiCss3 size="32" className={styles.icons} />
-            <DiJavascript1 size="32" className={styles.icons} />
-            <DiBootstrap size="32" className={styles.icons} />
-            <DiReact size="32" className={styles.icons} />
-            <DiGithubAlt size="32" className={styles.icons} />
-            <DiGit size="32" className={styles.icons} />
+            <span data-tooltip="HTML5"> <DiHtml5 size="32" className={styles.icons} /> </span>
+            <span data-tooltip="CSS3"> <DiCss3 size="32" className={styles.icons} /></span>
+            <span data-tooltip="Javascript"> <DiJavascript1 size="32" className={styles.icons} /></span>
+            <span data-tooltip="Bootstrap"> <DiBootstrap size="32" className={styles.icons} /></span>
+            <span data-tooltip="ReactJS">  <DiReact size="32" className={styles.icons} /></span>
+            <span data-tooltip="Github"> <DiGithubAlt size="32" className={styles.icons} /></span>
+            <span data-tooltip="Git"> <DiGit size="32" className={styles.icons} /></span>
           </div>
           <button
               className={styles.buttonLight}
