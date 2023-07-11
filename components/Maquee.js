cc2f3113f802c1e19e0158479218c57f5b411541
Wagner Paulo <wagstalos@hotmail.com>
2023-07-11 17:03:15 -0300

    fix: font

--- components/Maquee.js
+++ components/Maquee.js
@@ -18,28 +18,33 @@ export default function Marquee() {
       <img className="selo" src="images/SELO.svg" alt="" />
       <marquee behavior="" direction="">
         <div className="d-flex">
+
           <div>
             <RxSketchLogo size="18" color="#656AF5" className="icons-marquee" />{" "}
             <span>Frontend</span> <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiHtml5 size="20" color="#656AF5" className="icons-marquee" />
             <span>HTML</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiCss3 size="20" color="#656AF5" className="icons-marquee" />
             <span>CSS3</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiSass size="20" color="#656AF5" className="icons-marquee" />
             <span>SASS</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiJavascript1
@@ -50,33 +55,39 @@ export default function Marquee() {
             <span>Javascript</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiReact size="20" color="#656AF5" className="icons-marquee" />
             <span>ReactJS</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiBootstrap size="20" color="#656AF5" className="icons-marquee" />
             <span>Bootstrap</span>
             <RxLightningBolt size={16} />
           </div>
+
           <div>
             {" "}
             <DiGithubAlt size="20" color="#656AF5" className="icons-marquee" />
             <span>Github</span>
           </div>
+
           <div>
             {" "}
             <DiWordpress size="20" color="#656AF5" className="icons-marquee" />
             <span>Wordpress</span>
           </div>
+
           <div>
             {" "}
             <FaSalesforce size="20" color="#656AF5" className="icons-marquee" />
             <span>Salesforce</span>
           </div>
+          
         </div>
       </marquee>
     </div>
